a6d3dcc94b7b8b307cb36072c1d8ce5e649dc460
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-06-25 02:07:27 +0300

    Create evoxscript.sh

---
+++ evoxscript.sh
@@ -0,0 +1,10 @@
+rm -rf .repo/local_manifests
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs                                                                                                                                    git clone https://github.com/samsungexynos850/local_manifests  -b slsi .repo/local_manifests
+/opt/crave/resync.sh
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common  vendor/samsung/a21s-common -b lineage-22.2
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-22.2
+git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-22.2
+git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s device/samsung/a21s -b lineage-22.2
+git clone https://github.com/samsungexynos850/upstream_exynos850 kernel/samsung/exynos850 -b master
+source build/envsetup.sh 
+lunch lineage_a21s-bp2a-userdebug && m evolution
