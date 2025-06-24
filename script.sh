d4bf37bc1f364c5fd173d860a6df87993ee79a13
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-06-24 19:39:32 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs
-git clone https://github.com/samsungexynos850/local_manifests  -b slsi .repo/local_manifests
+git clone https://github.com/samsungexynos850/local_manifests -b slsi .repo/local_manifests
 /opt/crave/resync.sh
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common  vendor/samsung/a21s-common -b lineage-22.2
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-22.2
