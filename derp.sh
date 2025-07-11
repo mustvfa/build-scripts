0ecd931fbac203ee36a27e7571a23e4cd92d6b3e
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-11 21:11:59 +0300

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -19,6 +19,6 @@ git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common  
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-22.2
 git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-23.0
 git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s device/samsung/a21s -b lineage-23.0
-git clone https://github.com/mustafa-dgaf/upstream_exynos850 kernel/samsung/exynos850 -b lineage-23.0
+git clone https://github.com/samsungexynos850/upstream_exynos850 kernel/samsung/exynos850 -b lineage-23.0
 # 🍽️ Lunch time
 source build/envsetup.sh && lunch lineage_a21s-bp2a-userdebug && mka derp
