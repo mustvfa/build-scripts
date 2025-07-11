71ab2ca10125e162d82a8885b55a43bc4f529678
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-11 12:09:38 +0300

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Fresh manifest cleanup
-rm -rf .repo/local_manifests .repo/manifests .repo/manifest.xml
+rm -rf .repo/local_manifests
 # Initialize repo 
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/samsungexynos850/local_manifests -b aosp .repo/local_manifests
+#
+rm -rf prebuilts/clang/host/linux-x86
 # Sync
 /opt/crave/resync.sh
 # Clone device/vendor/kernel repositories
