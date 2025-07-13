e5d8824b046db287a82652329e721af68c4b1d7d
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-13 15:18:33 +0300

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "====================== BUILD STARTED ======================"
+croot && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh
+cd ../../..
 cd bionic && git revert b4ff0ab97d633f532480977b079ff26ba19ecd64 && cd ..
 # Build
 source build/envsetup.sh && lunch lineage_a21s-bp2a-userdebug && m evolution
