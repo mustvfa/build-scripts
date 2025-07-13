16b51584fcdab56994b1b028306140ad7c4c8e16
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-13 11:26:17 +0300

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "====================== BUILD STARTED ======================"
-rm -rf out/
+cd bionic && git revert b4ff0ab97d633f532480977b079ff26ba19ecd64 && cd ..
 # Build
 source build/envsetup.sh && lunch lineage_a21s-bp2a-userdebug && m evolution
