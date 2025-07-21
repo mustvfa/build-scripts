e5562d2a12a287e097b88e459cab5b6b0c0fb85a
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-21 22:24:57 +0300

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-# Fresh 
-rm -rf .repo/local_manifests
-# Initialize repo 
+echo "====================== BUILD STARTED ======================"
+
+# ROM source init
 repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
-git clone https://github.com/samsungexynos850/local_manifests -b slsi .repo/local_manifests
-#repo sync fix
+echo "==============================================================="
+echo "---------------------- Repo Init Success ----------------------"
+echo "==============================================================="
+
+# Local manifests
+git clone https://github.com/mustafa-dgaf/local_manifests- -b slsi .repo/local_manifests
+
+# Clean clang before sync
 rm -rf prebuilts/clang/host/linux-x86
-# Sync
+
+# Repo sync
 /opt/crave/resync.sh
-# Clone device/vendor/kernel repositories
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common  vendor/samsung/a21s-common -b lineage-22.2
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+echo "======================= Repo Sync Done =========================="
+
+#errors fixs
+git clone -b lineage-22.2 https://github.com/mustafa-dgaf/android_hardware_samsung_slsi-linaro_openmax hardware/samsung_slsi-linaro/openmax
+
+# Clone device/vendor/kernel trees
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-22.2
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-22.2
-git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-23.0
+git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-22.2
 git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s device/samsung/a21s -b lineage-23.0
 git clone https://github.com/samsungexynos850/upstream_exynos850 kernel/samsung/exynos850 -b lineage-23.0
-# 🍽️ Lunch time
-source build/envsetup.sh && lunch lineage_a21s-bp2a-userdebug && mka derp
+
+# kernel
+cd kernel/samsung/exynos850
+git reset --hard 39b0138abf46e230ed9d0fd6b9d01c606aa0379a
+cd ../../..
+
+echo "==============================================================="
+echo "----------- All Repositories Cloned Successfully -------------"
+echo "==============================================================="
+
+# Build
+source build/envsetup.sh && lunch lineage_a21s-bp2a-userdebug && m evolution
