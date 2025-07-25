b447535694c1032355324aebc660b9ebc8597d98
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-07-25 12:08:02 +0300

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -1,7 +1,3 @@
-rm -rf hardware/samsung/doze
-rm -rf hardware/samsung/AdvancedDisplay
-rm -rf hardware/samsung/hidl/fastcharge
-rm -rf hardware/samsung/hidl/touch
 chmod +r vendor/infinity/config/common_full_phone.mk
 #build
 source build/envsetup.sh && lunch infinity_a21s-userdebug && m bacon
