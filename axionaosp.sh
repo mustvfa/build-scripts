286c71c6fae26f88c625d130dcfa2b3bdfd2ac42
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-08-05 18:07:20 +0300

    Update axionaosp.sh

--- axionaosp.sh
+++ axionaosp.sh
@@ -12,7 +12,6 @@ git clone https://github.com/mustafa-dgaf/local_manifests- -b slsi .repo/local_m
 
 # Repo sync
 repo sync -j24
-repo sync -j4
 echo "======================= Repo Sync Done =========================="
 
 #dt
