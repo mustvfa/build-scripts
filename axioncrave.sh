5ae34c7253030c3a84868079a4e202ad5a4b3b40
mustafa <118094847+mustafa-dgaf@users.noreply.github.com>
2025-08-06 05:20:38 +0300

    Create axioncrave.sh

---
+++ axioncrave.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "====================== BUILD STARTED ======================"
+
+# ROM source init 
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+echo "==============================================================="
+echo "---------------------- Repo Init Success ----------------------"
+echo "==============================================================="
+
+#local manifests
+git clone https://github.com/mustafa-dgaf/local_manifests- -b slsi .repo/local_manifests
+
+# Crave specific error
+rm -rf prebuilts/clang/host/linux-x86
+
+# Repo sync
+curl https://raw.githubusercontent.com/accupara/docker-images/refs/heads/master/aosp/common/resync.sh | bash
+repo sync -j4
+echo "======================= Repo Sync Done =========================="
+
+#errors fixs
+rm -rf vendor/gapps/arm/Android.bp
+rm -rf vendor/gapps/arm64/Android.bp
+rm -rf build/
+git clone https://github.com/mustafa-dgaf/android_build build/
+
+#dt
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.0
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.0
+git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-23.0
+git clone https://github.com/mustafa-dgaf/android_device_samsung_a21s device/samsung/a21s -b lineage-23.0
+git clone https://github.com/mustafa-dgaf/upstream_exynos850 kernel/samsung/exynos850 -b lineage-23.0
+
+# fixs
+cd kernel/samsung/exynos850
+git reset --hard 39b0138abf46e230ed9d0fd6b9d01c606aa0379a
+cd ../../..
+
+echo "==============================================================="
+echo "----------- All Repositories Cloned Successfully -------------"
+echo "==============================================================="
+
+#build signing
+gk -s
+echo "==============================================================="
+echo "-------------------- The Build Got Signed----------------------"
+echo "==============================================================="
+#a lil sync
+axionSync
+#build
+source build/envsetup.sh && axion a21s va && ax -br -j24
+
+echo "==============================================================="
+echo "-----------The Build Is Done Succefully Build Mafia------------"
+echo "==============================================================="
