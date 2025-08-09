15b28de1f9ba668fa8f3d13a2f3a8264d47b6a79
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-08-09 19:33:33 +0300

    Update infinityA16.sh

--- infinityA16.sh
+++ infinityA16.sh
@@ -3,16 +3,13 @@
 echo "====================== BUILD STARTED ======================"
 
 # ROM source init 
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "==============================================================="
 echo "---------------------- Repo Init Success ----------------------"
 echo "==============================================================="
 
-# Crave specific error
-rm -rf prebuilts/clang/host/linux-x86
-
-# Repo sync
-curl https://raw.githubusercontent.com/accupara/docker-images/refs/heads/master/aosp/common/resync.sh | bash
+#repo sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 echo "======================= Repo Sync Done =========================="
 
