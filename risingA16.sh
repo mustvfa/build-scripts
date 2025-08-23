b82d3a0f4f12b6ae1ebd58e2c785003ab1a783ec
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-08-24 00:17:04 +0300

    delete echo cmds

--- risingA16.sh
+++ risingA16.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-echo "========== RisingOS Build Started =========="
-
 # Cleanup old local manifests
 rm -rf .repo/local_manifests
 
 # Initialize RisingOS repo
 repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
-echo "========== Repo Init Completed =========="
 
 # Clone local manifests
 git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
 
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j4
-echo "========== Repo Sync Done =========="
 
 #errors fixs
 rm -rf vendor/rising
@@ -30,8 +26,6 @@ git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/s
 git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-23.0
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b rise
 git clone https://github.com/mustvfa/upstream_exynos850 kernel/samsung/exynos850 -b lineage-23.0
-
-echo "========== All Sources Cloned Successfully =========="
 
 # Setup and build
 source build/envsetup.sh
