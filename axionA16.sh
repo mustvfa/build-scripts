68a807aeab96bc4255bf7b61942ce6f844f725c8
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-08-25 16:03:14 +0300

    Update and rename axioncrave.sh to axionA16.sh

---
+++ axionA16.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "====================== BUILD STARTED ======================"
+
+# ROM source init 
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+echo "==============================================================="
+echo "---------------------- Repo Init Success ----------------------"
+echo "==============================================================="
+
+#local manifests
+git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
+
+# Crave specific error
+rm -rf prebuilts/clang/host/linux-x86
+
+# Repo sync
+curl https://raw.githubusercontent.com/accupara/docker-images/refs/heads/master/aosp/common/resync.sh | bash
+repo sync -j4
+echo "======================= Repo Sync Done =========================="
+
+#errors fixs
+rm -rf vendor/gapps/arm/Android.bp
+rm -rf vendor/gapps/arm64/Android.bp
+
+#dt
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.0
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.0
+git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-23.0
+git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b axion
+git clone https://github.com/mustvfa/upstream_exynos850 kernel/samsung/exynos850 -b axion
+
+echo "==============================================================="
+echo "----------- All Repositories Cloned Successfully -------------"
+echo "==============================================================="
+#build
+source build/envsetup.sh && axionSync
+axion a21s va && ax -br -j24
+
+rm -rf out/target/product/a21s/lineage_a21s-ota.zip
+echo "==============================================================="
+echo "-----------The Build Is Done Succefully Build Mafia------------"
+echo "==============================================================="
--- axioncrave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf out/
-#build
-source build/envsetup.sh && axion a21s va && axionSync && ax -br -j24
-
-rm -rf out/target/product/a21s/lineage_a21s-ota.zip
-echo "==============================================================="
-echo "-----------The Build Is Done Succefully Build Mafia------------"
-echo "==============================================================="
