0191f070deab0d50a9e0892af95f0498bc932b29
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-08-25 21:40:02 +0300

    changes to make it work on local machine

--- evoxA16.sh
+++ evoxA16.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-echo "====================== BUILD STARTED ======================"
 
 # ROM source init
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-echo "==============================================================="
-echo "---------------------- Repo Init Success ----------------------"
-echo "==============================================================="
 
 # Local manifests
 git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
 
-# Clean clang before sync
-rm -rf prebuilts/clang/host/linux-x86
-
 # Repo sync
-/opt/crave/resync.sh
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
-echo "======================= Repo Sync Done =========================="
 
 #errors fixs
 rm -rf hardware/samsung/hidl/touch
@@ -32,10 +24,6 @@ git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/s
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b evox
 git clone https://github.com/mustvfa/upstream_exynos850 kernel/samsung/exynos850 -b master
 
-echo "==============================================================="
-echo "----------- All Repositories Cloned Successfully -------------"
-echo "==============================================================="
-#build signing
 git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh
 cd ../../..
 
