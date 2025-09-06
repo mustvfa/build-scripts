6ce39cad6ffc700d69183a4fc959ba36b8e30c6e
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-09-06 12:34:51 +0300

    Update infinityA16.sh

--- infinityA16.sh
+++ infinityA16.sh
@@ -5,7 +5,7 @@ echo "====================== BUILD STARTED ======================"
 # ROM source init 
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "==============================================================="
-echo "---------------------- Repo Init Success ----------------------"
+echo "---------------------- Repo Init bomb Success ----------------------"
 echo "==============================================================="
 
 #repo sync
