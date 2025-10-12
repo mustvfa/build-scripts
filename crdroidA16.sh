f6062129ea75a7d1e92d1c68aa97ed4b8f04ba14
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-10-12 16:25:28 +0300

    change to official repos

--- crdroidA16.sh
+++ crdroidA16.sh
@@ -20,11 +20,11 @@ rm -rf  hardware/samsung_slsi-linaro/openmax
 git clone -b lineage-22.2 https://github.com/mustvfa/android_hardware_samsung_slsi-linaro_openmax hardware/samsung_slsi-linaro/openmax
 
 #dt
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.0
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.0
-git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b los-23.0
-git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b los-23.0
-git clone https://github.com/mustvfa/upstream_exynos850 kernel/samsung/exynos850 -b master
+git clone https://github.com/crdroidandroid/android_device_samsung_a21s-common device/samsung/a21s-common -b 16.0
+git clone https://github.com/crdroidandroid/android_device_samsung_a21s device/samsung/a21s -b 16.0
+git clone https://github.com/crdroidandroid/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b 16.0
+git clone https://github.com/crdroidandroid/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b 16.0
+git clone https://github.com/crdroidandroid/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b 16.0
 
 echo "==============================================================="
 echo "----------- All Repositories Cloned Successfully -------------"
