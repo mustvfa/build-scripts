6bad631d500cdf608e5aabdf3c841dc69ae71234
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-10-13 14:07:41 +0300

    infinityA16.sh: rework

--- infinityA16.sh
+++ infinityA16.sh
@@ -8,6 +8,9 @@ echo "==============================================================="
 echo "---------------------- Repo Init bomb Success ----------------------"
 echo "==============================================================="
 
+#clone manifests
+git clone https://github.com/mustvfa/local_manifests -b slsi .repo/local_manifests
+
 #repo sync
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
@@ -18,27 +21,20 @@ rm -rf hardware/samsung
 rm -rf vendor/infinity
 git clone -b 16 https://github.com/ProjectInfinity-X/vendor_infinity vendor/infinity
 
-#alt to manifest
-git clone -b lineage-22.2 https://github.com/LineageOS/android_device_samsung_slsi_sepolicy device/samsung_slsi/sepolicy
-git clone -b lineage-22.2 https://github.com/tryinsmth/android_hardware_samsung hardware/samsung
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal hardware/samsung_slsi/scsc_wifibt/wifi_hal
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_config hardware/samsung_slsi-linaro/config
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exynos hardware/samsung_slsi-linaro/exynos
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_exynos5 hardware/samsung_slsi-linaro/exynos5
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_graphics hardware/samsung_slsi-linaro/graphics
-git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_samsung_slsi-linaro_interfaces hardware/samsung_slsi-linaro/interfaces
+# a16 fix
+rm -rf  hardware/samsung_slsi-linaro/openmax
 git clone -b lineage-22.2 https://github.com/mustvfa/android_hardware_samsung_slsi-linaro_openmax hardware/samsung_slsi-linaro/openmax
 
+#some errors fixs
 git clone https://github.com/ProjectInfinity-X/device_infinity_sepolicy device/infinity/sepolicy -b 16
 git clone https://github.com/ProjectInfinity-X/hardware_samsung_nfc hardware/samsung/nfc
+
 #dt
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.0
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.0
 git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b infx
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b infinityx
-git clone https://github.com/mustvfa/upstream_exynos850 kernel/samsung/exynos850 -b master
+git clone https://github.com/mustvfa/upstream_exynos850 kernel/samsung/exynos850 -b master-stable
 
 echo "==============================================================="
 echo "----------- All Repositories Cloned Successfully -------------"
