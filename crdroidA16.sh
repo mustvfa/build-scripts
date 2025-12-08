8c2ca55eb2d98b41a0acfa610ff6c7a54435ac10
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-12-08 15:22:32 +0200

    Clean up crdroidA16.sh

--- crdroidA16.sh
+++ crdroidA16.sh
@@ -16,16 +16,12 @@ rm -rf kernel/samsung/exynos850
 rm -rf .repo/local_manifests
 
 #manifest
-git clone https://github.com/mustvfa/local_manifests -b slsi .repo/local_manifests
+git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
 
 # Repo sync
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j4
 echo "======================= Repo Sync Done =========================="
-
-# a16 fix
-rm -rf  hardware/samsung_slsi-linaro/openmax
-git clone -b lineage-22.2 https://github.com/mustvfa/android_hardware_samsung_slsi-linaro_openmax hardware/samsung_slsi-linaro/openmax
 
 #dt
 git clone https://github.com/crdroidandroid/android_device_samsung_a21s-common device/samsung/a21s-common -b 16.0
@@ -41,7 +37,3 @@ echo "==============================================================="
 #build
 git clone git@github.com:crdroidandroid/crdroid-priv.git vendor/lineage-priv
 source build/envsetup.sh && brunch a21s userdebug
-
-echo "==============================================================="
-echo "-----------The Build Is Done Succefully Build Mafia------------"
-echo "==============================================================="
