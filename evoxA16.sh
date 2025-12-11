73481f40e3e8241e96e4b709fc154ca80dec6b1f
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-12-11 21:11:48 +0200

    Update repository URLs and sync commands in script

--- evoxA16.sh
+++ evoxA16.sh
@@ -3,10 +3,10 @@
 # ROM source init
 repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
 # Local manifests
-git clone https://github.com/samsungexynos850/local_manifests -b lineage-23.1 .repo/local_manifests
+git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
 
 # Repo sync
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+/opt/crave/resync.sh
 repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
 
 #errors fixs
@@ -17,8 +17,8 @@ rm -rf hardware/samsung/hidl/livedisplay
 # Clone device/vendor/kernel trees
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.1
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.1
-git clone https://github.com/LineageOS/android_device_samsung_a21s-common device/samsung/a21s-common -b lineage-23.1
-git clone https://github.com/LineageOS/android_device_samsung_a21s device/samsung/a21s -b lineage-23.1
+git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b evox
+git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b evox
 git clone https://github.com/LineageOS/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b lineage-23.1
 
 git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh
