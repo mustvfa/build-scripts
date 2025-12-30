d20177a4572c216f41e9c1fb0e04628a01ff5651
mustvfa <118094847+mustvfa@users.noreply.github.com>
2025-12-30 21:18:43 +0200

    ready to go qpr1 yay

--- axionA16.sh
+++ axionA16.sh
@@ -18,7 +18,7 @@ sed -i '/fastcharge/d' device/samsung_slsi/sepolicy/common/vendor/file_contexts
 #dt
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.1
 git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.1
-git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b los-23.0
+git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b axion
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b axion
 git clone https://github.com/LineageOS/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b lineage-23.1
 
@@ -27,8 +27,9 @@ echo "----------- All Repositories Cloned Successfully -------------"
 echo "==============================================================="
 #build
 source build/envsetup.sh && axionSync
-axion a21s && ax -br
+axion a21s && gk -s
+ax -br
 
-rm -rf out/target/product/a21s/lineage_a21s-ota.zip
 echo "==============================================================="
 echo "-----------The Build Is Done Succefully Build Mafia------------"
+echo "==============================================================="
