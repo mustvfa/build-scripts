ebcf066a26610ecd961df2897a32f2501b218961
mustvfa <118094847+mustvfa@users.noreply.github.com>
2026-01-02 13:26:46 +0200

    qpr1 init: yes bomb

--- infinityA16.sh
+++ infinityA16.sh
@@ -16,28 +16,16 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 echo "======================= Repo Sync Done =========================="
 
-# errors fixs
-rm -rf hardware/samsung
-git clone https://github.com/LineageOS/android_hardware_samsung hardware/samsung -b lineage-23.0
-rm -rf hardware/samsung/hidl/fastcharge/
-rm -rf hardware/samsung/hidl/livedisplay/
-rm -rf vendor/infinity
-git clone -b 16 https://github.com/ProjectInfinity-X/vendor_infinity vendor/infinity
-
-# a16 fix
-rm -rf  hardware/samsung_slsi-linaro/openmax
-git clone -b lineage-22.2 https://github.com/mustvfa/android_hardware_samsung_slsi-linaro_openmax hardware/samsung_slsi-linaro/openmax
-
 #some errors fixs
 git clone https://github.com/ProjectInfinity-X/device_infinity_sepolicy device/infinity/sepolicy -b 16
 git clone https://github.com/ProjectInfinity-X/hardware_samsung_nfc hardware/samsung/nfc
 
 #dt
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.0
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.0
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.1
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.1
 git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b infx
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b infinityx
-git clone https://github.com/mustvfa/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b lineage-23.0
+git clone https://github.com/LineageOS/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b lineage-23.1
 
 echo "==============================================================="
 echo "----------- All Repositories Cloned Successfully -------------"
@@ -52,4 +40,4 @@ git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-templat
 cd ../../..
 
 #build
-source build/envsetup.sh && lunch infinity_a21s-userdebug && m bacon
+source build/envsetup.sh && lunch infinity_a21s-userdebug && m bacon -j$(nproc --all)
