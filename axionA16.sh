3ac4fd4e0508b3088f8c8e14d9943363de8ea0de
mustvfa <118094847+mustvfa@users.noreply.github.com>
2026-03-28 18:37:11 +0200

    yes

--- axionA16.sh
+++ axionA16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # init
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
 
 # local manifests
 git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
@@ -12,8 +12,8 @@ repo sync -j4
 echo "======================= Repo Sync Done =========================="
 
 #dt
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.1
-git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.1
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s-common vendor/samsung/a21s-common -b lineage-23.2
+git clone https://github.com/TheMuppets/proprietary_vendor_samsung_a21s vendor/samsung/a21s -b lineage-23.2
 git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/samsung/a21s-common -b axion
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b axion
 git clone https://github.com/LineageOS/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b lineage-23.2
@@ -23,7 +23,7 @@ echo "----------- All Repositories Cloned Successfully -------------"
 echo "==============================================================="
 #build
 source build/envsetup.sh && axionSync
-axion a21s gms core && gk -s
+axion a21s gms va && gk -s
 ax -br
 
 echo "==============================================================="
