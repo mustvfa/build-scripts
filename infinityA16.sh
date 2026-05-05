47f2b05d199d482ee4d2b57324850f72d325180b
mustvfa <118094847+mustvfa@users.noreply.github.com>
2026-05-05 08:09:17 +0300

    som stuff

--- infinityA16.sh
+++ infinityA16.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-# ____   ___  __  __ ____  
-#| __ ) / _ \|  \/  | __ ) 
-#|  _ \| | | | |\/| |  _ \ 
-#| |_) | |_| | |  | | |_) |
-#|____/ \___/|_|  |_|____/
-
-mkdir infx && cd infx
-
-echo "====================== BUILD STARTED ======================"
-
 # ROM source init 
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "==============================================================="
-echo "---------------------- Repo Init bomb Success ----------------------"
+echo "---------------------- Repo Init Success ----------------------"
 echo "==============================================================="
 
 #clone manifests
 git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
 
 #repo sync
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+curl https://raw.githubusercontent.com/accupara/docker-images/refs/heads/master/aosp/common/resync.sh | bash
+repo sync -j4
 echo "======================= Repo Sync Done =========================="
 
 #dt
@@ -43,9 +33,5 @@ sed -i '/fastcharge/d' device/samsung_slsi/sepolicy/common/vendor/file_contexts
 git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys && cd vendor/infinity-priv/keys && ./keys.sh
 cd ../../..
 
-#yes
-export HOME=/home/mustafa-build/infx/out
-mkdir -p $HOME
-
 #build
-source build/envsetup.sh && lunch infinity_a21s-userdebug && m bacon -j$(nproc --all)
+source build/envsetup.sh && lunch infinity_a21s-userdebug && m bacon  
