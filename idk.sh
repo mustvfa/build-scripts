aef7ef6e7e644209dde25c7f279523d5e5bdca78
mustvfa <118094847+mustvfa@users.noreply.github.com>
2026-05-10 15:38:58 +0200

    hmm

--- idk.sh
+++ idk.sh
@@ -6,17 +6,6 @@ PATCH_FILE=$PATCH_DIR/idk.patch
 mkdir -p "$PATCH_DIR"
 
 cat > "$PATCH_FILE" <<'EOF'
-From c084f876d0fe35ee8fde64282d7639fc87d4249f Mon Sep 17 00:00:00 2001
-From: tejas101k <tejassingh649@rediffmail.com>
-Date: Wed, 6 May 2026 05:42:59 +0530
-Subject: [PATCH] SystemUI: Fix custom clock scale clipping
-
----
- .../android/systemui/clocks/ClockStyle.java   | 19 +++++++++++++++++++
- 1 file changed, 19 insertions(+)
-
-diff --git a/packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java b/packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
-index 58d1c78583b7..79f4df59019e 100644
 --- a/packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
 +++ b/packages/SystemUI/src/com/android/systemui/clocks/ClockStyle.java
 @@ -29,6 +29,7 @@
@@ -69,4 +58,4 @@ index 58d1c78583b7..79f4df59019e 100644
 EOF
 
 # Apply the patch as a commit
-git -C frameworks/base am "$PATCH_FILE"
+git -C frameworks/base apply "$PATCH_FILE"
