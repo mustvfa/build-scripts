98aef1e6391b52fb188d9348ff4f0304f1dd1b08
mustvfa <118094847+mustvfa@users.noreply.github.com>
2026-08-16 03:05:19 +0300

    whatever

--- axionA16.sh
+++ axionA16.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # init
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs --depth
 
 # local manifests
 git clone https://github.com/mustvfa/local_manifests- -b slsi .repo/local_manifests
 
 # Repo sync
 curl https://raw.githubusercontent.com/accupara/docker-images/refs/heads/master/aosp/common/resync.sh | bash
-repo sync -j4
 echo "======================= Repo Sync Done =========================="
 
 #dt
@@ -18,14 +17,10 @@ git clone https://github.com/mustvfa/android_device_samsung_a21s-common device/s
 git clone https://github.com/mustvfa/android_device_samsung_a21s device/samsung/a21s -b axion
 git clone https://github.com/LineageOS/android_kernel_samsung_exynos850 kernel/samsung/exynos850 -b lineage-23.2
 
-echo "==============================================================="
-echo "----------- All Repositories Cloned Successfully -------------"
-echo "==============================================================="
+echo "============================ cloned ==============================="
 #build
 source build/envsetup.sh && axionSync
 axion a21s va && gk -s
 ax -br
 
-echo "==============================================================="
-echo "-----------------The Build Is Done Succefully------------------"
-echo "==============================================================="
+echo "========================= its done ok ============================="
